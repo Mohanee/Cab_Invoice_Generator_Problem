b3a401c6aaca20eef51eeced6b0e2382e482d726
Mohanee <mohanee99@gmail.com>
2020-10-31 00:30:08 +0530

    [mohanee] add method to calculate Total fare for multiple rides

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -6,11 +6,21 @@ namespace CabInvoiceGenerator
 {
     public class InvoiceGenerator
     {
+        /// <summary>
+        /// Initialising constant values to variable for cost and min fare
+        /// </summary>
         const int COST_PER_KM = 10;
         const int COST_PER_MIN = 1;
         const double MIN_FARE = 5;
 
-        public double FareCalculation(int time, int distance)
+
+        /// <summary>
+        /// Method to calculate total fare for a single ride
+        /// </summary>
+        /// <param name="time">total time travelled</param>
+        /// <param name="distance">total distance travelled</param>
+        /// <returns>Total Fare for single ride</returns>
+        public double SingleTripFareCalculation(int time, double distance)
         {
             if (distance <= 0 && time <= 0)
             {
@@ -21,6 +31,32 @@ namespace CabInvoiceGenerator
             else
             {
                 double totalAmt = distance * COST_PER_KM + time * COST_PER_MIN;
+                return (Math.Max(MIN_FARE, totalAmt));
+            }
+        }
+
+
+        /// <summary>
+        /// Method to calculate Total Fare for Multiple Rides
+        /// </summary>
+        /// <param name="time">Array of distance travelled per ride</param>
+        /// <param name="distance">Array of time taken per ride</param>
+        /// <returns>Sum total Fare of all the rides</returns>
+        public double MultipleTripFareCalculation(int[] time, double[] distance)
+        {
+            double totalAmt = 0;
+            if (distance.Length != time.Length && distance.Length==0)
+            {
+                Console.WriteLine("Some values are missing");
+                return (-1);
+            }
+
+            else
+            {
+                for (int i = 0; i < distance.Length; i++)
+                {
+                   totalAmt += distance[i] * COST_PER_KM + time[i] * COST_PER_MIN;
+                }
                 return (Math.Max(MIN_FARE, totalAmt));
             }
         }
--- Cab_Invoice_Generation_Test/UnitTest1.cs
+++ Cab_Invoice_Generation_Test/UnitTest1.cs
@@ -12,13 +12,32 @@ namespace Cab_Invoice_Generation_Test
             invoiceGenerator = new InvoiceGenerator();
         }
 
+        /// <summary>
+        /// To test whether MultipleTripFareCalculation method returns expected TotalFare for Multiple Rides
+        /// Given an input of array of distance and array of time travelled
+        /// </summary>
         [Test]
-        public void Given_Distance_and_Time_Should_Return_Fare()
+        public void Given_MultipleDistance_and_Time_Should_Return_Fare()
         {
-            int distance = 15; int time = 20;
-            double actualFare = invoiceGenerator.FareCalculation(distance, time);
+            double[] distance = { 15, 10, 5 }; int[] time = { 20, 10, 5 };
+            double actualFare = invoiceGenerator.MultipleTripFareCalculation(time, distance);
 
-            Assert.AreEqual(170, actualFare);
+            Assert.AreEqual(335.0, actualFare);
+        }
+
+
+        /// <summary>
+        /// To test whether the SingleTripFareCalculation method returns expected TotalFare
+        /// </summary>
+        [Test]
+        public void Given_SingleDistance_and_Time_Should_Return_Fare()
+        {
+            double distance1 = 10; int  time1 = 5;
+            double actualFare = invoiceGenerator.SingleTripFareCalculation(time1, distance1);
+            double actualFare2 = invoiceGenerator.SingleTripFareCalculation(2,0.2);         //considering minimum fare criteria
+
+            Assert.AreEqual(105, actualFare);
+            Assert.AreEqual(5, actualFare2);
         }
     }
 }
